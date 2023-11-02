0d02ca03d93057c6240b49c1855af8554720a7ea
Jayjeet1013 <jayjeet1013@gmail.com>
2023-11-02 15:03:27 +0530

    done all intial structure

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,14 +3,16 @@ import  ReactDOM  from 'react-dom/client';
 import { BrowserRouter as Router} from 'react-router-dom';
 import {ChainId,ThirdwebProvider} from '@thirdweb-dev/react'
 
+import App from './App';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
         <ThirdwebProvider desiredChainId={ChainId.Goerli}> 
           <Router>
-            <StateContextProvider>
+   
               <App />
-            </StateContextProvider>
+         
           </Router>
         </ThirdwebProvider> 
       )
